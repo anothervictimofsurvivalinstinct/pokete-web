d22f21997e67d02111bd4427f2207f36d80e618d
anothervictimofsurvivalinstinct <94010769+anothervictimofsurvivalinstinct@users.noreply.github.com>
2022-05-18 10:56:40 -0600

    Got rid of maximized
    maximized caused the overworld to be tiny compared to screen size

--- startapp.sh
+++ startapp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-exec /usr/bin/xterm -maximized -e /bin/sh -c 'cd /app && ./pokete.py'
-#if you want it in dark mode replace with: exec /usr/bin/xterm -maximized -bg black -fg white -e /bin/sh -c 'cd /app && ./pokete.py'
+exec /usr/bin/xterm -e /bin/sh -c 'cd /app && ./pokete.py'
+#if you want it in dark mode replace with: exec /usr/bin/xterm -bg black -fg white -e /bin/sh -c 'cd /app && ./pokete.py'
