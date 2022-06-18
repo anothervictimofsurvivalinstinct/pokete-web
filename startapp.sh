3fa61300b437765749c35bb18107821c455526bf
Victim <94010769+anothervictimofsurvivalinstinct@users.noreply.github.com>
2022-06-17 18:08:58 -0600

    made selection of dark or white mode more intuitive

--- startapp.sh
+++ startapp.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 exec /usr/bin/xterm -e /bin/sh -c 'cd /app && ./pokete.py'
-#if you want it in dark mode replace with: exec /usr/bin/xterm -bg black -fg white -e /bin/sh -c 'cd /app && ./pokete.py'
+#exec /usr/bin/xterm -bg black -fg white -e /bin/sh -c 'cd /app && ./pokete.py'
+#if you want it in dark mode uncomment above and comment out the original
